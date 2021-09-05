498724dfe5ef9bd17dbcd58fe228bd1bb4b3be69
sinri0809 <dev.1lliodqb@gmail.com>
2021-09-05 16:57:14 +0900

    sinri_lab_demo_ver.1_modal

---
+++ design/modal/modal.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  $('.--close').click(function (e) { 
+    $('.modal').fadeOut();
+  });
+  $('.--confirm').click(function (e) { 
+    $('.modal').fadeOut();
+  });
+}
